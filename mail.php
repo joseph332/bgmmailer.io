02bb38d581b0bd6d3b9d8dfd56cb056960f5d0f3
joseph332 <58849810+joseph332@users.noreply.github.com>
2024-04-07 23:30:09 +0800

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,8 +1,3 @@
 <?php
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
-require 'path/to/PHPMailer/src/Exception.php';
-require 'path/to/PHPMailer/src/PHPMailer.php';
-require 'path/to/PHPMailer/src/SMTP.php';
+echo "hello";
